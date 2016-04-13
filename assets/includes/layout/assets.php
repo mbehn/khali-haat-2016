1410a4cda4307ecff41f33695a354a256a602fa9
mbehn <matthew.behn@gmail.com>
2016-04-13 00:13:41 -0500

    added popup function in image gallery need to add toggles

--- assets/includes/layout/assets.php
+++ assets/includes/layout/assets.php
@@ -18,14 +18,5 @@
 	<script type="text/javascript" src="assets/jquery/sizzle/dist/sizzle.min.js"></script>	
 	<link rel="stylsheet" type="text/css" href="assets/bootstrap/dist/css/bootstrap.min.css">
 	<script type="text/javascript" src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
-
-	<!-- bootstrap CDN -->
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> -->
-
-	<!-- Optional theme -->
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous"> -->
-	
-	<!-- Latest compiled and minified JavaScript -->
-	
 </head>
 <body id="<?php echo $current_page["PageName"]; ?>">
--- assets/includes/layout/image_gallery.php
+++ assets/includes/layout/image_gallery.php
@@ -2,14 +2,31 @@
 $i = 1;
 $image_array_legth = 13;
 ?>
+<div id="image-gallery-modal" role="dialog">
+    <div>
+    	<div><span id="close-modal">&times;</a></div>
+    	<h4 id="selected-image-header">Image Header</h4>	
+      	<img id="highlighted-gallery-image" src="">
+    </div>
+</div>
 <div id="image-gallery-outer-container">
 	<div id="image-gallery-container">
 		<?php while($i <  $image_array_legth) { ?>
 		<div class="image_thumbnail_container" id="image_thumbnail_container_<?php echo $i ?>">
 			<div class="inner-image-container">
-				<a href="#"><img src="assets/images/gallery_images/albums/2016/april/sahara_show/<?php echo $i ?>.jpg"></a>
+				<img class="gallery-image" src="assets/images/gallery_images/albums/2016/april/sahara_show/<?php echo $i ?>.jpg">
 			</div>			
 		</div>
 		<?php $i = $i +1;} ?>
 	</div>
 </div>
+<script type="text/javascript">
+		$(".gallery-image").click(function() {
+			$("#highlighted-gallery-image").attr('src', this.src);
+			$("#image-gallery-modal").css('z-index', '999');
+			$("#image-gallery-modal").toggle('fade');
+		})
+		$("#close-modal").click(function() {
+			$("#image-gallery-modal").toggle('fade')
+		})
+</script>
